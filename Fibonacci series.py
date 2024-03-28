dfa99d856572d0f8b49c9f814bf72f080f2a0523
Nkwain219 <151454442+Nkwain219@users.noreply.github.com>
2024-03-28 07:53:29 +0100

    Add files via upload

--- Fibonacci series.py
+++ Fibonacci series.py
@@ -1,10 +1,12 @@
+num1=int(input("Enter the first number of the series:"))
+num2=int(input("Enter the second number of the series:"))
 def fibonacci_series(n):
     if(n<=0):
         return ''
     elif(n==1):
-        return '0'
+        return f"{num1}"
     else:
-        series=[0,1]
+        series=[num1,num2]
         while len(series)<n:
           next_digit=series[-1]+series[-2]
           series.append(next_digit)
